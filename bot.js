c412eca97c5684a5f4d99e6b2ae5550057db0b9b
Srijay Kasturi <skasturi@cadoth.net>
2014-04-28 11:44:19 -0400

    And now the logging chan works, yey

--- bot.js
+++ bot.js
@@ -1,16 +1,42 @@
 var irc = require('fwilson-irc-fork');
 var config = require('./config.json');
-var bot = new irc.Client('chat.freenode.net', config.nick, {channels: [config.chan], sasl: "true", userName: "olympicsbot", password: config.pass});
+var bot = new irc.Client('chat.freenode.net', config.nick, {channels: config.chan, sasl: "true", userName: "olympicsbot", password: config.pass});
 var trusted = config.trusted;
 var op = config.op;
+
+bot.addListener('error', function(msg) {
+    console.log('error: ' +  msg);
+});
+
+function log(msg) {
+    bot.say(config.logchan.chan, msg);
+}
 console.log("Starting bot.");
 
+
+bot.addListener('pm', function (nick, msg) {
+    log("<" + nick + "> " + msg);
+});
+
+//setTimeout(function() {}, 2000);
+//bot.join(config.logchan.chan + config.logchan.key);
+//console.log("Joining logchan");
+
 bot.on('raw', function(msg) {
+    if (msg.rawCommand == "NOTICE" && msg.args[0][0] != "#") {
+		log("-" + msg.nick + "- " + msg.args[1]);
+	//console.log("Notice has been sent by " + msg.args[0] + ", logging it to log chan");
+    }
+
     if (msg.rawCommand == "PRIVMSG") {
 	var nick = msg.nick;
 	var cloak = msg.host;
 	var chan = msg.args[0];
 	
+	/*if(msg.args[0][0] != "#") {
+	    bot.say(config.logchan.chan, "<" + chan + "> " + msg.args[1]);
+	    console.log("Private Message has been sent, logging it to log chan");
+	}*/
 	if(msg.args[1][0] == config.prefix) {
 	    msg.args[1] = msg.args[1].replace(config.prefix, config.nick + ": ");
 	}
