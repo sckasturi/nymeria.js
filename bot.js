30ba2be29b3f57ec3326b279ff53416e09419670
Srijay Kasturi <skasturi@cadoth.net>
2014-05-03 22:38:32 -0400

    Removing a messed up package.json, and fixing other stuff

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ function sendmemo(msg) {
     var memoarr = {
         "nick": [],
         "send": [],
-	    "memo": []
+        "memo": []
     }
     var nick = msg.nick;
     var chan = msg.args[0];
@@ -34,126 +34,130 @@ function sendmemo(msg) {
 
     for (i = 0; i < memos.length; i++) {
         var memo = memos[i].split(",", 2);
-	memoarr.nick.push(memo[0]);
-	memoarr.send.push(memo[1]);
-	memoarr.memo.push(memos[i].replace("{0},{1},".format(memo[0], memo[1]), ""));
+    memoarr.nick.push(memo[0]);
+    memoarr.send.push(memo[1]);
+    memoarr.memo.push(memos[i].replace("{0},{1},".format(memo[0], memo[1]), ""));
     }
     
     var memonum = memoarr.nick.indexOf(nick);
     if(memonum >= 0) {
         bot.say(chan, "{0}, you have a memo: <{1}> {2}".format(nick, memoarr.send[memonum], memoarr.memo[memonum]));
-	var rm = "sed -i\".bak\" \'" + (i - 1) + "d\' memo.txt";
-	console.log(rm);
-	exec(rm, function(error, stdout, stderr) {});
-	
+    var rm = "sed -i\".bak\" \'" + (i - 1) + "d\' memo.txt";
+    exec(rm, function(error, stdout, stderr) {});
+    
     }   
 }
 
 function runcmd(cmd, msg) {
-	var nick = msg.nick;
-	var cloak = msg.host;
-	var chan = msg.args[0];
-	var text = msg.args[1].split(" ");
-	var cmd = text[1].toLowerCase();
+    var nick = msg.nick;
+    var cloak = msg.host;
+    var chan = msg.args[0];
+    var text = msg.args[1].split(" ");
+    var cmd = text[1].toLowerCase();
 
-    if(cmd == "help" || cmd == "list") {
-    	bot.say(chan, "Normal Commands: [{0}], Trusted Commands: [{1}], Op Commands: [{2}], Onwer Commands: [{3}]".format(config.cmd.toString(), config.trusted.cmd.toString(), config.op.cmd.toString(), config.owner.cmd.toString()));
-    }
-    else if(cmd == "trustedcheck" || cmd == "tcheck") {
-	   bot.say(chan, nick + ": Yes! You are trusted!");
-	}
-	else if(cmd == "opcheck") {
-	   bot.say(chan, nick + ": Yes! You are an op in {0}!".format(chan));
-	}
-	else if(cmd == "ocheck" || cmd == "ownercheck") {
-		bot.say(chan, nick + ": Yes! You are my owner!")
-	}
-	else if(cmd == "msg") {
-		if(text[2].split(",").length == 1 && text[2][0] == "#") {
-	        var send = msg.args[1].replace("{0}: msg {1} ".format(config.nick, text[2]), "");
-	        bot.say(text[2], send);
-	    }
-	}
-	else if(cmd == "act") {
-		if(text[2].split(",").length == 1 && text[2][0] == "#") {
-            var send = msg.args[1].replace("{0}: act {1} ".format(config.nick, text[2]), "");
-	        bot.action(text[2], send);
-	    }
-	}
-	else if(cmd == "op") {
-	    bot.send('MODE', chan, '+o', nick);
-	}
-	else if(cmd == "deop") {
-	    bot.send('MODE', chan, '-o', nick);
-	}
-	else if(cmd == "mode") {
-	    var send = msg.args[1].replace("{0}: mode".format(config.nick) , "");
-	    bot.conn.write("MODE " + chan + send + "\n");
-	}
-	else if(cmd == "do") {
-	    var send = msg.args[1].replace("{0}: do".format(config.nick) , "");
-	    console.log(send);
-	    bot.conn.write(send + "\n");
-	}
-	else if(cmd == "ddate") {
-	    //bot.say(chan, function(reply, data, args) { run("ddate", [], reply); })'
-	    exec("ddate", function(error, stdout, stderr) { bot.say(chan, stdout); });
-	}
-	else if(cmd == "memo") {
-	    var memo = msg.args[1].replace(config.nick + ": memo " + text[2] + " ", "");
-	    if(text[2].split(",").length == 1) {
-	        bot.say(chan, "okay, sending your memo");
-	        exec("echo {0},{1},{2} >> memo.txt".format(text[2], nick, memo), 
-	            function(error, stdout, stderr) { log("[{0}] {1} sends a memo to {2}: {3}".format(chan, nick, text[2], memo)); });
-	    }
-	}
-	else if(cmd == "oracle") {
-	    var morgan = fs.readFileSync('./morganstarot.txt').toString().split("\n");
-            bot.say(chan, morgan[Math.floor(Math.random() * morgan.length)]);
-	}
-	else if(cmd == "coffee") {
-	    if(text.length == 2) { bot.action(chan, "hands {0} a steaming cup of delicious coffee".format(nick)); }
-	    else { bot.action(chan, "hands {0} a steaming cup of delicious coffee".format(text[2])); }
-	}
-	else if(cmd == "tea") {
-	    if(text.length == 2) { bot.action(chan, "hands {0} a nice cup of tea.".format(nick)); }
-	    else { bot.action(chan, "hands {0} a nice cup of tea".format(text[2])); }
-	}
-	else if(cmd == "cookie") {
-	    if(text.length >= 3) { bot.action(chan, "gets {0} a plate of cookies and a glass of milk".format(text[2])); }
-	}
-	else if(cmd == "goat") {
-	    if(text.length >= 3) { bot.say(chan, "{0}'s goat walks by and kicks {1}".format(nick, text[2])); }
-	}
-	else if(cmd == "wolf") {
-	    if(text.length >= 3) { bot.say(chan, "{0}'s wolf walks by and noms {1}".format(nick, text[2])); }
+    case "help":
+        bot.say(chan, "Normal Commands: [{0}], Trusted Commands: [{1}], Op Commands: [{2}], Onwer Commands: [{3}]".format(config.cmd.join(", "), config.trusted.cmd.join(", "), config.op.cmd.join(", "), config.owner.cmd.join(", ")));
+        break;
+    case "list":
+        runcmd("help");
+        break;
+    case "trustedcheck":
+        bot.say(chan, nick + ": Yes! You are trusted!");
+        break;
+    case  "tcheck":
+        runcmd("trustedcheck");
+    case "opcheck":
+        bot.say(chan, nick + ": Yes! You are an op in {0}!".format(chan));
+        break;
+    case "ocheck":
+        runcmd("ownercheck");
+    case "ownercheck":
+        bot.say(chan, nick + ": Yes! You are my owner!")
+        break;
+    case "msg":
+        if(text[2].split(",").length == 1 && text[2][0] == "#") {
+            var send = msg.args[1].replace("{0}: msg {1} ".format(config.nick, text[2]), "");
+            bot.say(text[2], send);
         }
-	else if(cmd == "bear") {
-	    if(text.length >= 3) { bot.say(chan, "{0}'s bear walks by and gives {1} a huge bear hug".format(nick, text[2])); }
-	}
-
-    else if(cmd == "kitty") {
+        break;
+    case "act":
+        if(text[2].split(",").length == 1 && text[2][0] == "#") {
+            var send = msg.args[1].replace("{0}: act {1} ".format(config.nick, text[2]), "");
+            bot.action(text[2], send);
+        }
+        break;
+    case "op":
+        bot.send('MODE', chan, '+o', nick);
+        break;
+    case "deop":
+        bot.send('MODE', chan, '-o', nick);
+        break;
+    case "mode":
+        var send = msg.args[1].replace("{0}: mode".format(config.nick) , "");
+        bot.conn.write("MODE " + chan + send + "\n");
+        break;
+    case "do":
+        var send = msg.args[1].replace("{0}: do".format(config.nick) , "");
+        console.log(send);
+        bot.conn.write(send + "\n");
+        break;
+    case "ddate":
+        exec("ddate", function(error, stdout, stderr) { bot.say(chan, stdout); });
+        break;
+    case "memo":
+        var memo = msg.args[1].replace(config.nick + ": memo " + text[2] + " ", "");
+        if(text[2].split(",").length == 1) {
+            bot.say(chan, "okay, sending your memo");
+            exec("echo {0},{1},{2} >> memo.txt".format(text[2], nick, memo), 
+                function(error, stdout, stderr) { log("[{0}] {1} sends a memo to {2}: {3}".format(chan, nick, text[2], memo)); });
+        }
+        break;
+    case "oracle":
+        var morgan = fs.readFileSync('./morganstarot.txt').toString().split("\n");
+            bot.say(chan, morgan[Math.floor(Math.random() * morgan.length)]);
+        break;
+    case "coffee":
+        if(text.length == 2) { bot.action(chan, "hands {0} a steaming cup of delicious coffee".format(nick)); }
+        else { bot.action(chan, "hands {0} a steaming cup of delicious coffee".format(text[2])); }
+        break;
+    case "tea":
+        if(text.length == 2) { bot.action(chan, "hands {0} a nice cup of tea.".format(nick)); }
+        else { bot.action(chan, "hands {0} a nice cup of tea".format(text[2])); }
+        break;
+    case "cookie":
+        if(text.length >= 3) { bot.action(chan, "gets {0} a plate of cookies and a glass of milk".format(text[2])); }
+        break;
+    case "goat":
+        if(text.length >= 3) { bot.say(chan, "{0}'s goat walks by and kicks {1}".format(nick, text[2])); }
+        break;
+    case "wolf":
+        if(text.length >= 3) { bot.say(chan, "{0}'s wolf walks by and noms {1}".format(nick, text[2])); }
+            break;
+    case "bear":
+        if(text.length >= 3) { bot.say(chan, "{0}'s bear walks by and gives {1} a huge bear hug".format(nick, text[2])); }
+        break;
+    case "kitty":
        if(text.length >= 3) { bot.say(chan, "{0}'s kitty climbs into {1}'s lap, curls up, and falls asleep".format(nick, text[2])); }
-    }
-    else if(cmd == "penguin") {
+        break;
+    case "penguin":
         if(text.length >= 3) { bot.say(chan, "{0}'s penguin waddles by and slaps {1}!".format(nick, text[2])); }
-    }
-    else if(cmd == "kekse" || cmd == "keks") {
+        break;
+    case "kekse":
         if(text.length >= 3) { bot.action(chan, "holt {0}  einen Teller Kekse und ein Glas Milch".format(text[2])); }
-    }
-    else if(cmd == "ping") {
+        break;
+    case "ping":
         bot.say(chan, "Pong!");
-    }
-    else if(cmd == "quit") {
-    	bot.quit();
-    }
-    else if(cmd == "nick") {
+        break;
+    case "quit":
+        bot.quit();
+        break;
+    case "nick":
         config.nick = text[2]
-    	bot.send("NICK", text[2]);
-    }
-    else if(cmd == "botsnack") {
-    	bot.action(chan, "noms happily :3");
-    }
+        bot.send("NICK", text[2]);
+        break;
+    case "botsnack":
+        bot.action(chan, "noms happily :    3");
+    break;
 }
 
 function log(msg) {
@@ -170,40 +174,40 @@ bot.addListener('pm', function (nick, msg) {
 
 bot.on('raw', function(msg) {
     if (msg.rawCommand == "NOTICE" && msg.args[0][0] != "#") {
-		log("-" + msg.nick + "- " + msg.args[1]);
+    log("-" + msg.nick + "- " + msg.args[1]);
     }
 
     if (msg.rawCommand == "PRIVMSG") {
-	var nick = msg.nick;
-	var cloak = msg.host;
-	var chan = msg.args[0];
-	
-	if(msg.args[1][0] == config.prefix) {
-	    msg.args[1] = msg.args[1].replace(config.prefix, config.nick + ": ");
-	}
-	
-	var text = msg.args[1].split(" ");
-	var cmd = text[1]
+    var nick = msg.nick;
+    var cloak = msg.host;
+    var chan = msg.args[0];
+    
+    if(msg.args[1][0] == config.prefix) {
+        msg.args[1] = msg.args[1].replace(config.prefix, config.nick + ": ");
+    }
+    
+    var text = msg.args[1].split(" ");
+    var cmd = text[1]
         
-	sendmemo(msg);
+    sendmemo(msg);
 
-	if(text[0] == config.nick + ":" || text[0] == config.nick + ",") {
-	    if(config.trusted.cmd.indexOf(cmd) >= 0) {
-	    if(config.trusted.cloaks.indexOf(cloak) >= 0) {
-        	runcmd(cmd, msg);      
-	    }
-	    else {
-	        bot.say(chan, nick + ": You're not the boss of me!");
-	    }}
+    if(text[0] == config.nick + ":" || text[0] == config.nick + ",") {
+        if(config.trusted.cmd.indexOf(cmd) >= 0) {
+        if(config.trusted.cloaks.indexOf(cloak) >= 0) {
+            runcmd(cmd, msg);
+        }
+        else {
+            bot.say(chan, nick + ": You're not the boss of me!");
+        }}
 
-	    else if(config.op.cmd.indexOf(cmd) >= 0) {
-	    if(Object.keys(config.op.cloaks).indexOf(cloak) >= 0) {
-	    if(config.op.cloaks[cloak].indexOf(chan) >= 0) {
-	        runcmd(cmd, msg);
-	    }}
-	    else {
-	        bot.say(chan, nick + ": You're not the boss of me!");
-	    }}
+        else if(config.op.cmd.indexOf(cmd) >= 0) {
+        if(Object.keys(config.op.cloaks).indexOf(cloak) >= 0) {
+        if(config.op.cloaks[cloak].indexOf(chan) >= 0) {
+            runcmd(cmd, msg);
+        }}
+        else {
+            bot.say(chan, nick + ": You're not the boss of me!");
+        }}
         if(config.owner.cmd.indexOf(cmd) >= 0) {
         if(config.owner.cloaks.indexOf(cloak) >= 0) {
             runcmd(cmd, msg);
@@ -214,7 +218,7 @@ bot.on('raw', function(msg) {
         if(config.cmd.indexOf(cmd) >= 0) {
              runcmd(cmd, msg);
         }
-	}
+    }
     }
 });
         
